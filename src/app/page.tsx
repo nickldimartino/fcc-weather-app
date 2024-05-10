29e993be49aaa8d81558dfacd4d12e4da61b9580
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-05-10 17:37:12 -0400

    Date and Time display from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import axios from "axios";
 import Navbar from "./components/Navbar";
 import { useQuery } from "react-query";
+import { format, parseISO } from "date-fns";
 
 interface WeatherDetail {
   dt: number;
@@ -69,13 +70,32 @@ export default function Home() {
     }
   );
 
-  console.log("data", data)
+  const firstData = data?.list[0];
 
-  if (isLoading) return "Loading...";
+  console.log("data", data);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+        <p className="animate-bunce">Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="fle flex-col grap- bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-1 pt-4">
+        <section>
+          <div>
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+              <p className="text-lg">({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})</p>
+            </h2>
+          </div>
+        </section>
+        <section></section>
+      </main>
     </div>
   );
 }
