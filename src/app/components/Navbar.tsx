a6e4b003de10c0c6244c8b319a5135eb158952b5
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-05-11 14:26:19 -0400

    Location search bar functionality

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,58 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { MdWbSunny, MdMyLocation, MdOutlineLocationOn } from "react-icons/md";
 import SearchBar from "./SearchBox";
+import { Trykker } from "next/font/google";
+import axios from "axios";
+import { ImInsertTemplate } from "react-icons/im";
 
 type Props = {};
 
 export default function Navbar({}: Props) {
+  const [city, setCity] = useState("");
+  const [error, setError] = useState("");
+
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  async function handleInputChange(value: string) {
+    setCity(value);
+    if (value.length >= 3) {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/find?q=${value}e&APPID=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+        );
+
+        const suggestions = response.data.list.map((item: any) => item.name);
+        setSuggestions(suggestions);
+        setError("");
+        setShowSuggestions(true);
+      } catch (error) {
+        setSuggestions([]);
+        setShowSuggestions(false);
+      }
+    } else {
+      setSuggestions([]);
+      setShowSuggestions(false);
+    }
+  }
+
+  function handleSuggestionClick(value: string) {
+    setCity(value);
+    setShowSuggestions(false);
+  }
+
+  function handleSubmitSearch(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (suggestions.length === 0) {
+      setError("Location not found");
+    } else {
+      setError("");
+      setShowSuggestions(false);
+    }
+  }
+
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
@@ -17,10 +65,55 @@ export default function Navbar({}: Props) {
           <MdOutlineLocationOn className="text-3xl" />
           <p className="text-slate-900/80 text-sm">Boston</p>
           <div>
-            <SearchBar />
+            <SearchBar
+              value={city}
+              onSubmit={(e) => handleSubmitSearch(e)}
+              onChange={(e) => handleInputChange(e.target.value)}
+            />
+            <SuggestionBox
+              {...{
+                showSuggestions,
+                suggestions,
+                handleSuggestionClick,
+                error,
+              }}
+            />
           </div>
         </section>
       </div>
     </nav>
+  );
+}
+
+function SuggestionBox({
+  showSuggestions,
+  suggestions,
+  handleSuggestionClick,
+  error,
+}: {
+  showSuggestions: boolean;
+  suggestions: string[];
+  handleSuggestionClick: (item: string) => void;
+  error: string;
+}) {
+  return (
+    <>
+      {((showSuggestions && suggestions.length > 1) || error) && (
+        <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
+          {error && suggestions.length < 1 && (
+            <li className="cursor-pointer p-1 rounded hover:bg-gray-200"></li>
+          )}
+          {suggestions.map((item, i) => (
+            <li
+              key={i}
+              onClick={() => handleSuggestionClick(item)}
+              className="cursor-pointer p-1 rounded hover:bg-gray-200"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
